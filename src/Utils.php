d9e71eb1078ba89826732cc2fb6fddce9417efe5
Olga Zayarnaya <olmianser@yandex.ru>
2022-11-23 21:29:39 +0300

    edit Utils.php and UtilsTest.php

--- src/Utils.php
+++ src/Utils.php
@@ -5,5 +5,5 @@ namespace Hexlet\Phpunit\Utils;
 //Эта функция переворачивает переданную строку
 function reverseString($string)
 {
-	return implode(array_reverse(str_split($string)));
+    return implode(array_reverse(str_split($string)));
 }
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,20 +3,21 @@
 namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Hexlet\Phpunit\Utils\reverseString;
 
 //класс UtilsTest наследует класс TestCase
 //имя класса совпадает с именем файла
 class UtilsTest extends TestCase
 {
-	//Метод, функция определенная внутри класса
-	//Должна начинаться со слова test
-	//public - чтобы PHPUnit мог вызывать этот тест снаружи
-	public function testReverse(): void
-	{
-		//сначала идет ожидаемое значение (expected)
-		//и только потом актуальное (actual)
-		$this->assertEquals('', reverseString(''));
-		$this->assertEquals('olleh', reverseString('hello'));
-	}
+    //Метод, функция определенная внутри класса
+    //Должна начинаться со слова test
+    //public - чтобы PHPUnit мог вызывать этот тест снаружи
+    public function testReverse(): void
+    {
+        //сначала идет ожидаемое значение (expected)
+        //и только потом актуальное (actual)
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
 }
